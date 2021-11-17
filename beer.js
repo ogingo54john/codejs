8a859f06edd1e7e9ee86d4fd70642abcc8b927b5
ogingo54john <60035498+ogingo54john@users.noreply.github.com>
2021-11-17 17:15:29 +0300

    Add files via upload

---
+++ beer.js
@@ -0,0 +1,10 @@
+var n=99
+while(n>=1){
+  // console.log(n);
+console.log(n + " bottle of beer,take 1 down remain with " + (n-1));
+n--;
+}
+//  99 bottle of beer,take 1 down remain with 98
+// 98 bottle of beer,take 1 down remain with 97
+//  97 bottle of beer,take 1 down remain with 96
+//  96 bottle of beer,take 1 down remain with 95

1098195bf0ab6371c0e5b04245f01e64ead24bc8
ogingo54john <60035498+ogingo54john@users.noreply.github.com>
2021-11-17 17:17:42 +0300

    Add files via upload

---
+++ fizzbuzzbyforloop.js
@@ -0,0 +1,13 @@
+for(i=1;i<=100;i++){
+    console.log(i);
+     if(i%3===0 && i%5===0){
+        console.log("fizzBuzz");
+    }else if(i%5===0){
+       console.log("fizz");
+    }else if(i%3===0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
